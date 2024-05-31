f437505f0733075620b8d1f48897bb3dbd787179
Navaneethraot <youremail@gmail.com>
2024-05-31 11:25:29 +0530

    pushing to git hub

---
+++ tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket= "navaneeth-dev"
+key =  "multi-env-tfvars"
+region =  "us-east-1"
+dynamodb_table= "navaneeth-dev"
---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket= "navaneeth-prod"
+key =  "multi-env-tfvars"
+region =  "us-east-1"
+dynamodb_table= "navaneeth-prod"
